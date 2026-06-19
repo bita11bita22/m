751961138f50e73c1e275d12add81d8213c143c5
bita11bita22 <bita11bita22@atomicmail.io>
2026-06-19 23:40:22 +0330

    Add files via upload

--- run.sh
+++ run.sh
@@ -8,16 +8,5 @@ nginx
 
 sleep 1
 
-# ساخت کانفیگ Cloudflare WARP در صورت عدم وجود
-if [ ! -f /app/warp_profile.conf ]; then
-    echo "Registering Cloudflare WARP..."
-    cd /app
-    /usr/local/bin/wgcf register --accept-tos
-    /usr/local/bin/wgcf generate
-    # تغییر نام فایل برای استفاده در پایتون
-    mv wgcf-profile.conf /app/warp_profile.conf
-    rm -f wgcf-account.toml
-fi
-
 echo "Starting Panel..."
 exec python3 /app/panel.py
